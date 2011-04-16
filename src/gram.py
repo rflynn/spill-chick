69ea99b5e8ce6b2fcf54a88033f583c165a31089
pizza <parseerror@gmail.com>
2011-04-15 22:49:30 -0400

    add \n to TokRgx so we can parse entired files if we want, much faster; simplify ngram storage

--- src/gram.py
+++ src/gram.py
@@ -12,7 +12,7 @@ Group:
 	numbers and any punctuation
 		group things like dates, times, ip addresses, etc. into a single token
 """
-TokRgx = re.compile('\d+(?:[^\w\s]+\d+)*|\w+')
+TokRgx = re.compile('\d+(?:[^\w\s]+\d+)*|\w+|\n')
 def tokenize(str):
 	return re.findall(TokRgx, str.lower())
 
@@ -33,33 +33,45 @@ class TokenizerTest(unittest.TestCase):
 store corpus ngrams
 """
 class Grams:
-	def __init__(self, w, ngmax=2, f=None):
+	def __init__(self, w, ngmax=3, f=None):
 		self.words = w
 		self.ngmax = ngmax
-		self.ngrams = (defaultdict(int), defaultdict(int)) # ngram id -> frequency
+		self.ngrams = ( # ngram id -> frequency
+			None,
+			None,
+			defaultdict(int),
+			defaultdict(int),
+		)
+		self.surround = defaultdict(dict) # (x,y,z) : [(x,z)] : [y]
 		if f:
 			self.add(f)
 	def freq(self, ng):
 		if type(ng) == str:
-			return self.ngrams[0][ng]
-		return self.ngrams[len(ng)-1][ng]
+			return self.words.freq(ng)
+		if len(ng) == 1:
+			return self.words.freq(ng[0])
+		return self.ngrams[len(ng)][ng]
 	# given an iterable 'f', tokenize and produce a {word:id} mapping and ngram frequency count
 	def add(self, f):
-		leftover = []
+		if type(f) == list:
+			contents = '\n'.join(f)
+		else:
+			contents = f.read()
+			if type(contents) == bytes:
+				contents = contents.decode('utf8')
 		try:
-			for line in f:
-				if type(line) == bytes:
-					line = line.decode('utf8')
-				tok = tokenize(line)
-				self.words.addl(tok)
-				# ngram frequency
-				# len=1
-				for t in tok:
-					self.ngrams[0][t] += 1
-				# len=2
-				for i in range(0, len(tok)-1):
-					ng = tuple(tok[i:i+2])
-					self.ngrams[1][ng] += 1
+			toks = tokenize(contents)
+			self.words.addl(toks)
+			for x,y in zip(toks, toks[1:]):
+				self.ngrams[2][(x,y)] += 1
+			for x,y,z in zip(toks, toks[1:], toks[2:]):
+				self.ngrams[3][(x,y,z)] += 1
+				try:
+					self.surround[(x,z)][y] += 1
+				except KeyError:
+					self.surround[(x,z)][y] = 1
+			#for x,y,z,zz in zip(toks, toks[1:], toks[2:], toks[3:]):
+				#self.ngrams[(x,y,z,zz)] += 1
 		except UnicodeDecodeError:
 			t,v,tb = sys.exc_info()
 			traceback.print_tb(tb)
