17dbd288b9ac98584a5c000446613629fe22d55c
pizza <parseerror@gmail.com>
2011-04-18 03:12:26 -0400

    merge alternatives less stupidly. variables more descriptive. passes tests.

--- src/test.py
+++ src/test.py
@@ -63,15 +63,9 @@ if __name__ == '__main__':
     """
 
     def inter_token(toks, freq, g):
-        print('toks=',toks)
         if sum(map(len, toks)) <= 12:
             s = list(algo.splits(toks, freq, g))
-            print('s[',len(s),']=',s[:1000])
-            js0 = s
-            js1 = [(toks, sum([freq.get(t,0) for t in toks])) for toks in js0]
-            js2 = sorted(js1, key=lambda x:x[1], reverse=True)
-            print('inter_token()=',js2[:5])
-            return js2
+            return s
         else:
             print('too long for exhaustive')
             return []
@@ -99,6 +93,7 @@ if __name__ == '__main__':
 
         print('w.correct(naieve)=',w.correct('naieve'))
         print('g.freq((i,know))=',g.freq(('i','know')))
+        print('g.freq((it,use))=',g.freq(('it','use')))
 
 
         # load test cases
@@ -113,20 +108,27 @@ if __name__ == '__main__':
                     after = re.sub('\s*#.*', '', after.rstrip()) # replace comments
                     Tests.append(([before],[after]))
 
+        def rsort(l, **kw):
+            return sorted(l, reverse=True, **kw)
+        def rsortn(l, n):
+            return rsort(l, key=itemgetter(n))
+        def sortn(l, n):
+            return sorted(l, key=itemgetter(n))
 
         def alternatives(w, p, g, d, t):
             """
             given tok ('token', line, index) return a list of possible alternative tokens.
             only return alternatives that within the realm of popularity of the original token.
             """
-            edit = w.similar(t)[:30]
+            edit = w.similar(t)
             phon = p.soundsLike(t,g)
-            uniq = set(edit + phon)
+            uniq = edit | set(phon)
             minpop = lambda x: round(log(g.freqs(x)))
-            alts = [(x,levenshtein(t,x),minpop(x)) for x in uniq if minpop(x) > 0]
-            alts = sorted(alts, key=itemgetter(2), reverse=True)
-            alts = sorted(alts, key=itemgetter(1))
-            return alts
+            alt = [(x,levenshtein(t,x),minpop(x)) for x in uniq if minpop(x) > 0]
+            #alt = rsort_nth(alt, 2)
+            #alt = sort_nth(alt, 1)
+            alt2 = [x[0] for x in alt]
+            return set(alt2)
 
         alt_know = alternatives(w,p,g,None,'know')
         print('alt(know)=',alt_know)
@@ -136,81 +138,89 @@ if __name__ == '__main__':
 
         passcnt = 0
         for str,exp in Tests:
-            print('doc=', str)
             d = Doc(str, w)
+            print('doc=', d.tok)
 
             # start out like a regular spellchecker
             # address unknown tokens (ngram size 1) first
             ut = list(d.unknownToks())
             print('unknownToks=',ut)
+            utChanges = [(u, w.correct(u[0])) for u in ut]
+            print('utChanges=',utChanges)
+            d.applyChanges(utChanges)
 
-            # now the hard part.
-            # locate uncommon n-gram sequences which may indicate grammatical errors
-            # see if we can determine better replacements for them given their context
+            """
+            now the hard part.
+            locate uncommon n-gram sequences which may indicate grammatical errors
+            see if we can determine better replacements for them given their context
+            """
 
             # order n-grams by unpopularity
             ngsize = min(3, d.totalTokens())
             while ngsize >= min(3, d.totalTokens()):
+                ngsize = min(3, d.totalTokens())
                 print('ngsize=',ngsize)
-                print('ngramfreq=',list(d.ngramfreq(g,1)))
+                print('ngram(1) freq=',list(d.ngramfreq(g,1)))
 
                 # locate the least-common ngrams
-                ng0 = list(takewhile(lambda x:x[1] <= 1,
-                    sorted(d.ngramfreq(g, ngsize), key=lambda x:x[1])))
-                print('d.ngramfreq=', ng0[:20])
-                ng0x = [x[0] for x in ng0]
-                print('ng0x=',ng0x)
+                least_common = sortn(d.ngramfreq(g, ngsize), 1)
+                print('least_common=', least_common[:20])
+                if least_common == []:
+                    break
+
+                target_ngram = least_common[0]
+                print('target_ngram=',target_ngram)
+                toks = [x[0] for x in target_ngram[0]]
+                print('toks=',toks)
 
                 # find potential alternative tokens for the tokens in the unpopular ngrams
-                alt = dict([(t[0],[]) for ng in ng0x for t in ng])
+                alt = dict([(t, alternatives(w,p,g,d,t)) for t in toks])
                 print('alt=',alt)
-                # calculate alternatives for each unique token
-                for t in alt.keys():
-                    print('alt(',t,')=',alternatives(w,p,g,d,t))
-                    alt[t] = [x[0] for x in alternatives(w,p,g,d,t)]
-                # map alternatives back to tokens
-                ng0b = [tuple(alt[t[0]] for t in n) for n in ng0x]
-                print('alternatives...', ng0b)
 
-                # consider inter-token possibilities as well.
-                # not quite sure when/how to invoke this or integrate it.
-                # when we have an unknown token/ngram? always?
-                tokenlists = [tuple(t[0] for t in n) for n in ng0x]
-                print('tokenlists=',tokenlists)
-                if tokenlists != []:
-                    intertok = inter_token(tokenlists[0], w.frq, g)
-                    def list_intersect(l,m):
-                        return list(filter(lambda x:x in l, m))
-                    # list all ngrams containing partial matches for our ngram
-                    partial = g.ngram_like(tokenlists[0])
-                    print('partial=', partial)
-                    # intersect with potential alternatives
-                    print('ng0x before partial=',ng0x)
-                    print('alt before partial=',alt)
+                # list all ngrams containing partial matches for our ngram
+                part = g.ngram_like(toks)
+                print('part=', part)
 
-                    ng0b = [tuple(list_intersect(p, alt[k])
-                                for p,k in zip(partial, toks))
-                                    for toks in tokenlists]
-                    print('intersected alternatives=',ng0b)
+                """
+                part & alt
+                part is based on our corpus and alt based on token similarity.
+                an intersection between the two means we've found as good a candidate as we'll get.
+                """
+                part_pop = [[p for p in pa if p in alt[t]] for t,pa in zip(toks, part)]
+                print('part_pop=', part_pop)
 
-                def mapstar(f, l):
-                    return [f(*x) for x in l]
+                if any(p == [] for p in part_pop):
+                    """
+                    incomplete intersection. either we just haven't seen it or they've truly mangled it.
+                    """
+                    intertok = inter_token(toks, w.frq, g)
+                    print('intertok=', intertok)
+                    if intertok != []:
+                        part_pop = [(x,) for x in intertok[0]]
 
-                # given our possible alternatives in ng0b, find the 
-                ng0c = [list(product(*n)) for n in ng0b]
-                print('alt ngrams...', ng0c[:100], '...')
-                ng0d = [[(n, g.freq(n), sum(mapstar(levenshtein,zip(n,tl))))
-                            for n,tl in zip(l,cycle(tokenlists))]
-                                for l in ng0c]
-                print('magic=', ng0d)
-                ng0d = [sorted(x, key=lambda x:x[1]/max(1,x[2]), reverse=True) for x in ng0d]
-                #ng0d = [list(takewhile(lambda x:x[1] > 0, x)) for x in ng0d]
+                    if intertok == []:
+                        for i in range(len(part_pop)):
+                            if part_pop[i] == []:
+                                dec = [(t, g.freqs(t), levenshtein(t, toks[i]))
+                                        for t in alt[toks[i]] if t != toks[i]]
+                                part_pop[i] = [x[0] for x in sortn(dec, 2)][:3]
+                        print("part_pop'=", part_pop)
 
-                print('popular...', ng0d)
+                partial = list(product(*part_pop))
+                print('partial=', partial)
 
-                # present our potential revisions
-                proposedChanges = list(zip(ng0x, ng0d))
-                print('proposedChanges...', proposedChanges)
+                best = partial
+                print('best=',best)
+
+                if best:
+                    # present our potential revisions
+                    proposedChanges = list(zip(target_ngram[0], best[0]))
+                    print('proposedChanges...', proposedChanges)
+                    res = d.demoChanges(proposedChanges)
+                    print(res)
+                    d.applyChanges(proposedChanges)
+
+                """
                 def merge_changes(changes):
                     revs = []
                     # isolate only the actual changes
@@ -233,12 +243,12 @@ if __name__ == '__main__':
                 finalChanges = sorted(finalChanges, key=lambda x:x[0][1]) # sort by line
                 finalChanges = sorted(finalChanges, key=lambda x:x[0][2]) # then by index
                 print('finalChanges=', finalChanges)
-                res = d.demoChanges(finalChanges)
-                print(res)
-                passcnt += res == exp
-                print('-----------','pass' if res == exp else 'fail','------------')
+                """
                 ngsize -= 1
-                #assert res == exp
+
+            passcnt += d.lines == exp
+            print('-----------','pass' if d.lines == exp else 'fail','------------')
+            #assert res == exp
         print('Tests %u/%u passed.' % (passcnt, len(Tests)))
 
     from sys import argv
