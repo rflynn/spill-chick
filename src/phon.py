a51ae86b3d20fa9747ee08a47e2378838ebccefd
pizza <parseerror@gmail.com>
2011-04-18 16:38:44 -0400

    add support for mapping lists of tokens to sounds and extracting similar phrases

--- src/phon.py
+++ src/phon.py
@@ -27,6 +27,7 @@ class Phon:
 					continue
 				line = line.strip().lower()
 				word, phon = line.split('  ')
+				phon = re.sub('\d+', '', phon)
 				self.words.add(word)
 				self.word[word].append(phon)
 				self.phon[phon].append(word)
@@ -43,6 +44,31 @@ class Phon:
 					l.append((x,fr))
 		return [w for w,fr in sorted(l, key=lambda x:x[1], reverse=True)]
 
+	def phraseSound(self, toks):
+		def head(l):
+			return l[0] if l else None
+		s = [head(self.word.get(t,[''])) for t in toks]
+		print('phraseSound(',toks,')=',s)
+		if not all(s):
+			return []
+		# nuke numbers, join into one string
+		t = ' '.join([re.sub('\d+', '', x) for x in s])
+		# nuke consecutive duplicate sounds
+		u = re.sub('(\S+) \\1 ', '\\1 ', t)
+		v = u.split()
+		print('phraseSound2=',v)
+		return v
+
+	def soundsToWords(self, snd):
+		if snd == []:
+			yield []
+		for j in range(1, len(snd)+1):
+			t = ' '.join(snd[:j])
+			words = self.phon.get(t)
+			if words:
+				for s in self.soundsToWords(snd[j:]):
+					yield [words] + s
+
 if __name__ == '__main__':
 
 	def words(str):
@@ -57,4 +83,12 @@ if __name__ == '__main__':
 
 	print(P.word['there'])
 	print(P.phon[P.word['there'][0]])
+
+	P.phraseSound(['making','mistake'])
+	P.phraseSound(['may','king','mist','ache'])
+	x = P.phraseSound(['making','miss','steak'])
+	from itertools import product
+	for f in P.soundsToWords(x):
+		print(f)
+		#print(list(product(*f)))
 
