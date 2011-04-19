ade6078860c57b0d47630586bcbbc2a5ec7d73cd
pizza <parseerror@gmail.com>
2011-04-19 01:22:50 -0400

    add line index to ngrampos tuple to aid in locating ngram context

--- src/doc.py
+++ src/doc.py
@@ -39,7 +39,7 @@ class Doc:
 			ll = []
 			for t in toks:
 				tpos = line.index(t, tpos)
-				ll.append((t, lcnt, tpos))
+				ll.append((t, lcnt, len(ll), tpos))
 				tpos += len(t)
 			self.tok[-1] += ll
 
@@ -70,14 +70,59 @@ class Doc:
 			ng2 = tuple(t[0] for t in ng)
 			yield (ng, g.freq(ng2))
 
-	"""
-	Modify replacement word 'y' to match the capitalization of existing word 'x'
-	(foo,bar) -> bar
-	(Foo,bar) -> Bar
-	(FOO,bar) -> BAR
-	"""
+
+	def ngram_prev(self, ngpos):
+		_,line,index,_ = ngpos
+		if index == 0:
+			if line == 0:
+				return None
+			line -= 1
+			index = len(self.tok[line])-1
+		else:
+			index -= 1
+		return self.tok[line][index]
+
+	def ngram_next(self, ngpos):
+		_,line,index,_ = ngpos
+		if index == len(self.tok[line])-1:
+			if line == len(self.tok)-1:
+				return None
+			line += 1
+			index = 0
+		else:
+			index += 1
+		return self.tok[line][index]
+
+	def ngram_context(self, ngpos, size):
+		"""
+		given an ngram and a size, return a list of ngrams that contain
+		one or more members of ngram
+		    c d e
+                a b c d e f g
+		"""
+		before, ng = [], ngpos[0]
+		for i in range(size):
+			ng = self.ngram_prev(ng)
+			if not ng:
+				break
+			before.insert(0, ng)
+		after, ng = [], ngpos[-1]
+		for i in range(size):
+			ng = self.ngram_next(ng)
+			if not ng:
+				break
+			after.append(ng)
+		return before + list(ngpos) + after
+
+
 	@staticmethod
 	def matchCap(x, y):
+		"""
+		Modify replacement word 'y' to match the capitalization of existing word 'x'
+		(foo,bar) -> bar
+		(Foo,bar) -> Bar
+		(FOO,bar) -> BAR
+		"""
 		if x == x.lower():
 			return y
 		elif x == x.capitalize():
@@ -86,11 +131,12 @@ class Doc:
 			return y.upper()
 		return y
 
-	"""
-	given an ngram containing position data, replace corresponding data in lines with 'mod'
-	"""
 	def applyChange(self, lines, ngpos, mod, off):
-		o,l,pos = ngpos
+		"""
+		given an ngram containing position data, replace corresponding data
+		in lines with 'mod'
+		"""
+		o,l,idx,pos = ngpos
 		pos += off[l]
 		end = pos + len(o)
 		ow = lines[l][pos:end]
@@ -107,11 +153,11 @@ class Doc:
 			self.docwords[mod] += 1
 		return (lines, off)
 
-	"""
-	given a list of positional ngrams and a list of replacements, apply the changes
-	and return a copy of the updated file
-	"""
 	def demoChanges(self, changes):
+		"""
+		given a list of positional ngrams and a list of replacements,
+		apply the changes and return a copy of the updated file
+		"""
 		lines = self.lines[:]
 		off = [0] * len(lines)
 		for change in changes:
