be435aa3d848119f85de6620a3c684ea2efc66f3
pizza <parseerror@gmail.com>
2011-04-20 01:53:21 -0400

    check in mostly-working scripts to fetch/manipulate the huge google books ngram set

---
+++ data/corpus/google-ngrams/extract.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""
+once fetch.py has grabbed a set of ngrams, I parse out a subset and generate CSV
+files that will be compatible with sqlite
+given our lists of 'x y z\tcnt', extract, parse and dump to an sqlite database
+"""
+
+import os, re
+
+Ids = {'UNKNOWN':0, '$PROPERNOUN':1, '$NUMBER':2}
+
+def getid(word):
+	global Ids
+	w = word.lower()
+	try:
+		return Ids[w]
+	except:
+		cnt = len(Ids)+1
+		Ids[w] = cnt
+		return cnt
+
+wtf = list(os.walk('.'))
+dirpath, dirnames, filenames = wtf[0]
+for filename in sorted(filenames):
+	if not filename.endswith('-2008.list.gz'):
+		continue
+	dst = str.replace(filename,'list.gz','ids.gz')
+	if os.path.exists(dst):
+		continue
+	print('filename=',filename)
+	with os.popen('gunzip -dc ' + filename, 'r') as gunzip:
+		contents = '\n' + gunzip.read()
+	with os.popen('gzip -c - > ' + dst, 'wb') as gz:
+		for x,y,z,cnt in re.findall('\n([^\d\W]+) ([^\d\W]+) ([^\d\W]+)\t(\d+)', contents):
+			xid, yid, zid = getid(x), getid(y), getid(z)
+			gz.write('%u,%u,%u,%u\n' % (xid, yid, zid, int(cnt)))
+	print('break')
+	break
+
+with os.popen('gzip -c - > ids.csv.gz', 'wb') as gz:
+	Ids = sorted(Ids.items(), key=lambda x:x[1])
+	for word,wid in Ids:
+		gz.write('%s,%u\n' % (word, wid))
+
---
+++ data/corpus/google-ngrams/fetch.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""
+fetch Google Books' 3-ary ngrams.
+run me, then extract.py
+enumerate, download, extract, filter and delete files
+"""
+
+import datetime
+
+def log(what, msg):
+	print('%s %s %s' % (datetime.datetime.now(), what, msg))
+
+import urllib2
+
+def download(url, dst):
+	CHUNK = 2 * 1024 * 1024
+	req = urllib2.urlopen(url)
+	with open(dst, 'wb') as fp:
+		while 1:
+			chunk = req.read(CHUNK)
+			if not chunk: break
+			fp.write(chunk)
+	return dst
+
+import re, collections
+import os
+
+def extract(filename, gzfile):
+	CHUNK = 8 * 1024 * 1024
+	with os.popen('unzip -p ' + filename) as fd:
+		d = {}
+		while 1:
+			txt = fd.read(CHUNK)
+			if not txt: break
+			# ! ! Along       2008    4
+			d.update(re.findall('\n([^\t]+)\t2008\t(\d+)', txt))
+		with os.popen('gzip -c - > ' + gzfile, 'wb') as out:
+			for k in sorted(d.keys()):
+				out.write('%s\t%s\n' % (k, d[k]))
+
+def delete(filename):
+	try:
+		os.remove(filename)
+	except:
+		pass
+
+def urls():
+	for n in range(5, 200):
+		yield 'http://commondatastorage.googleapis.com/books/ngrams/books/googlebooks-eng-all-3gram-20090715-%u.csv.zip' % n
+
+def url2filename(url):
+	return url[url.rfind('/')+1:]
+
+def filename2gz(filename):
+	return filename + '-2008.list.gz'
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv) > 1 and sys.argv[1] == '--run':
+		for url in urls():
+			try:
+				dst = url2filename(url)
+				dstgz = filename2gz(dst)
+				if not os.path.exists(dstgz):
+					if not os.path.exists(dst):
+						log(url, 'download')
+						download(url, dst)
+					log(dst, 'extract')
+					extract(dst, dstgz)
+					log(dst, 'delete')
+			except urllib2.HTTPError, e:
+				print(e.reason)
+				print('continuing...')
+			finally:
+				delete(dst) # delete partial
