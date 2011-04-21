832147cf8b7a29e1fe3f513629510cb264f58787
pizza <parseerror@gmail.com>
2011-04-21 18:10:58 -0400

    fix setup's example invocation of testbin.py; also produce better test results

--- data/corpus/google-ngrams/setup.py
+++ data/corpus/google-ngrams/setup.py
@@ -3,7 +3,7 @@ How To Use
 
 $ python setup.py build
 $ sudo python setup.py install
-$ ./testbin.py
+$ time python3 -i < testbin.py
 
 $ python3 -i
 
@@ -12,7 +12,8 @@ $ python3 -i
 >>> [ng.word2id(w) for w in ['activities','as','buddhist']]
 >>> [ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']]
 >>> ng.find(5,6,7)
->>> ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])
+>>> [(ng.id2word(x), ng.id2word(y), ng.id2word(z), freq)
+	for x,y,z,freq in ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])]
 
 """
 
--- data/corpus/google-ngrams/testbin.py
+++ data/corpus/google-ngrams/testbin.py
@@ -4,21 +4,7 @@ ng = ngram3bin('word.bin','ngram3.bin')
 [ng.word2id(w) for w in ['activities','as','buddhist']]
 [ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']]
 ng.find(5,6,7)
-ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])
+# convert to ids, search, convert back to words
+[(ng.id2word(x), ng.id2word(y), ng.id2word(z), freq)
+	for x,y,z,freq in ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])]
 
-"""
-from ngram3bin import ngram3bin
-ng = ngram3bin('word.bin','ngram3.bin')
-print('ng=', ng)
-#print('word2id(a:4)=', ng.word2id('a'))
-print('word2id(activities:5)=', ng.word2id('activities'))
-#print('word2id(of:6)=', ng.word2id('of'))
-print('word2id(buddhist:43)', ng.word2id('buddhist'))
-#print('word2id(as:8)=', ng.word2id('as'))
-#print('word2id(bibliography:12)=', ng.word2id('bibliography'))
-word4 = ng.id2word(4)
-print('id2word(4)=', word4)
-f = ng.find(5,6,7)
-print('find(5,6,7)=', f)
-ng.find(7,8,9)
-"""
