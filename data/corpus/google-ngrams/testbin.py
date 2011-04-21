cdb72c63e305a6e64d16357300076caa75f4e83a
pizza <parseerror@gmail.com>
2011-04-21 18:02:03 -0400

    improve example usage

--- data/corpus/google-ngrams/testbin.py
+++ data/corpus/google-ngrams/testbin.py
@@ -1,3 +1,12 @@
+
+from ngram3bin import ngram3bin
+ng = ngram3bin('word.bin','ngram3.bin')
+[ng.word2id(w) for w in ['activities','as','buddhist']]
+[ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']]
+ng.find(5,6,7)
+ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])
+
+"""
 from ngram3bin import ngram3bin
 ng = ngram3bin('word.bin','ngram3.bin')
 print('ng=', ng)
@@ -12,3 +21,4 @@ print('id2word(4)=', word4)
 f = ng.find(5,6,7)
 print('find(5,6,7)=', f)
 ng.find(7,8,9)
+"""
