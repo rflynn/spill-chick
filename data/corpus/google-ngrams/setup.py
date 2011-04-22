a45f2392b3245f6beecc17181ad23b7662b062b7
pizza <parseerror@gmail.com>
2011-04-21 21:19:49 -0400

    take the test cases out of setup.py, that's what testbin.py is for

--- data/corpus/google-ngrams/setup.py
+++ data/corpus/google-ngrams/setup.py
@@ -5,16 +5,6 @@ $ python setup.py build
 $ sudo python setup.py install
 $ time python3 -i < testbin.py
 
-$ python3 -i
-
->>> from ngram3bin import ngram3bin
->>> ng = ngram3bin('word.bin','ngram3.bin')
->>> [ng.word2id(w) for w in ['activities','as','buddhist']]
->>> [ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']]
->>> ng.find(5,6,7)
->>> [(ng.id2word(x), ng.id2word(y), ng.id2word(z), freq)
-	for x,y,z,freq in ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])]
-
 """
 
 from distutils.core import setup, Extension
