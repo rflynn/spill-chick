30cb03a98cdb3ead6b7b4d0484cf2983b9976620
pizza <parseerror@gmail.com>
2011-04-21 21:20:23 -0400

    incorporate new functions and add word2id() test case so we don't crash when id not found

--- data/corpus/google-ngrams/testbin.py
+++ data/corpus/google-ngrams/testbin.py
@@ -1,10 +1,14 @@
 
 from ngram3bin import ngram3bin
 ng = ngram3bin('word.bin','ngram3.bin')
-[ng.word2id(w) for w in ['activities','as','buddhist']]
+ng.word2id('freq')
+ng.word2id('FDWD#$#$@#@')
+list(map(ng.word2id, ['activities','as','buddhist']))
+[ng.id2freq(ng.word2id(w)) for w in ['activities','as','buddhist']]
 [ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']]
-ng.find(5,6,7)
+ng.freq(4,22,215)
+ng.like(5,6,7)
 # convert to ids, search, convert back to words
 [(ng.id2word(x), ng.id2word(y), ng.id2word(z), freq)
-	for x,y,z,freq in ng.find(*[ng.word2id(w) for w in ['activities','as','buddhist']])]
+	for x,y,z,freq in ng.like(*[ng.word2id(w) for w in ['activities','as','buddhist']])]
 
