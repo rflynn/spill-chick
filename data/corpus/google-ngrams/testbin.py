8adc1f80698018e73764b265ab91835a4f33c720
pizza <parseerror@gmail.com>
2011-04-25 17:33:33 -0400

    add structure to testbin.py

--- data/corpus/google-ngrams/testbin.py
+++ data/corpus/google-ngrams/testbin.py
@@ -5,8 +5,8 @@ ng = ngram3bin('word.bin','ngram3.bin')
 ng.word2id('freq')
 ng.word2id('FDWD#$#$@#@')
 list(map(ng.word2id, ['activities','as','buddhist']))
-[ng.id2freq(ng.word2id(w)) for w in ['activities','as','buddhist']]
-[ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']]
+[ng.id2freq(ng.word2id(w)) for w in ['activities','as','buddhist']][:30]
+[ng.id2word(ng.word2id(w)) for w in ['activities','as','buddhist']][:30]
 ng.freq(4,22,215)
 ng.like(5,6,7)
 # convert to ids, search, convert back to words
@@ -20,7 +20,44 @@ print('word(idknow)')
 ng.id2word(idknow)
 assert 'know' == ng.id2word(ng.word2id('know'))
 assert ng.id2freq(ng.word2id('know')) == ng.wordfreq('know')
+
+# "bridge" missing made find a bug
 print('id(bridge)=', ng.word2id('bridge'))
 print('id2freq(bridge)=', ng.id2freq(ng.word2id('bridge')))
 print('wordfreq(bridge)=', ng.wordfreq('bridge'))
+
+# "didn" seems to be missing but shouldn't be...
+print('wordfreq(didn)=', ng.wordfreq('didn'))
+
+[(w,ng.word2id(w)) for w in ['didn','t','know']]
+ng.freq(*[ng.word2id(w) for w in ['didn','t','know']])
+
+# freq2
+(('didn','t'), ng.freq(*[ng.word2id(w) for w in ['didn','t']]))
+(('and','that'), ng.freq(*[ng.word2id(w) for w in ['and','that']]))
+(('a','mistake'), ng.freq(*[ng.word2id(w) for w in ['a','mistake']]))
+
+Test = [
+	'am fond of',
+	'am found of',
+	'i now that',
+	'i know that',
+	'is now that',
+	'future would undoubtedly',
+	'it it did',
+	'if it did',
+	'and then it',
+	'the united states',
+	'don t',
+	'don',
+	'dont',
+]
+for s in Test:
+	t = s.lower().split()
+	ids = [ng.word2id(w) for w in t]
+	frfunc = ng.freq if len(ids) > 1 else ng.id2freq
+	print((t, 'freq:', frfunc(*ids), 'ids:', ids))
+	assert all(ng.id2word(ng.word2id(w)) == w for w in t)
+
+ng.like((ng.word2id('don'),))
 
